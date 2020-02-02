e6b3fc643afaea6f96eb3359bfa5ea2ed3e55bc5
Marcin Moch <marcio.moch@gmail.com>
2020-02-02 03:50:50 +0100

    Updated users page

--- src/components/User.js
+++ src/components/User.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export const User = ({
   name,
+  login,
+  registerDate
 }) => {
   return (
     <div className="group">
       <div className="group__content group-content">
         <div className="group-content__title">
-          {name}
+          {name} | @{login} | od: {registerDate}
         </div>
       </div>
     </div>
